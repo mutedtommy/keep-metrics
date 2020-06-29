315930512444dc379e7f9ca26f93372973924180
navneetlakra <navneet.lakra@gmail.com>
2020-06-29 14:00:15 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//import express from 'express';
+//import db from './db/db';
+
+const express = require("express")
+const db = require("./db/db")
+// Set up the express app
+const app = express();
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
